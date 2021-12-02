31c0fed1364e287c7eff2d85efdb0dd33874d518
Slimane RECHDI <47180175+recs12@users.noreply.github.com>
2021-12-02 21:20:27 +0100

    add one line

--- ViewUsersEplanVersion.ps1
+++ ViewUsersEplanVersion.ps1
@@ -1,1 +1,4 @@
 Get-WmiObject -Class Win32_Product | where vendor -eq "Oracle Corporation" | select Name, Version
+<#
+Invoke-Command -ComputerName Server01, Server02 -ScriptBlock {Get-UICulture}
+#>
